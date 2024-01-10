9814a0122398348f7d2a0106d22ba8b937420763
구태호 <0307rnxogh@daum.net>
2024-01-10 18:48:34 +0900

    React.useEffect

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-import {useState} from "react";
+import {useState, useEffect} from "react";
 
 
 function App() {
   const [counter,setValue] = useState(0);
   const onClick = () => setValue((a) => a+1);
-  console.log("render");
+  console.log("i run all the time");
+  const iRunOnlyOnce = () => {
+    console.log("i run only once");
+  };
+  useEffect(iRunOnlyOnce, []);
   return (
     <div>
       <h1 >{counter}</h1>
