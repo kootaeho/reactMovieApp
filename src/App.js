b3de43721008d6cf14a401c42ef241a6b7263a89
구태호 <0307rnxogh@daum.net>
2024-01-10 17:32:29 +0900

    React.styleModule

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
+ import Button from "./Button";
 
 function App() {
   return (
     <div>
-      <h1>
-        Welcome back!
-      </h1>
+      <h1>Welcome back!</h1>
+      <Button text={"Button"}/>
     </div>
   );
 }
