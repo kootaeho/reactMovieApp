7d7be1dca5c76114a5720c291044897b59220799
구태호 <0307rnxogh@daum.net>
2024-01-10 18:27:32 +0900

    .fixedBefore

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
- import Button from "./Button";
- import styles from "./App.module.css";
+import {useState} from "react";
+
 
 function App() {
+  const [counter,setValue] = useState(0);
+  const onClick = () => setValue((a) => a+1);
+  console.log("render");
   return (
     <div>
-      <h1 className ={styles.title}>Welcome back!</h1>
-      <Button text={"Button"}/>
+      <h1 >{counter}</h1>
+      <button onClick={onClick}>click me</button>
     </div>
   );
 }
