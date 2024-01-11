23bc11702db95de0efaa6f5bc5cd30079d6beee5
kth <0307rnxogh@daum.net>
2024-01-11 23:27:48 +0900

    React.btc

--- src/App.js
+++ src/App.js
@@ -1,31 +1,25 @@
 import {useState, useEffect} from "react";
 
-function App() {
-  const [toDo,setToDo] = useState("");
-  const [toDos, setToDos] = useState([]);
-  const onChange = (event) => setToDo(event.target.value);
-  const onSubmit = (event) => {
-    event.preventDefault();
-    if (toDo === ""){
-      return;
-    }
-    setToDos((currentArray) => [toDo, ...currentArray]);
-    setToDo("");
-  }
-  return (
+function App(){
+  const [loading, setLoading] = useState(true);
+  const [coins, setCoins] = useState([]);
+  useEffect(() => {
+  fetch("https://api.coinpaprika.com/v1/tickers").then(response => response.json()).then((json) => {setCoins(json); setLoading(false)});
+  }, [])
+  return ( 
   <div>
-    <h1>My To Dos ({toDos.length})</h1>
-    <form onSubmit={onSubmit}>
-      <input onChange = {onChange} value = {toDo} type="text" placeholder="Write your to do"/>
-      <button>Add To Do</button>
-    </form>
-    <hr />
+    <h1>The Coins</h1>
+    {loading ? <strong>Loading...</strong>:null}
     <ul>
-      {toDos.map((item, index) => <li key = {index}>{item}</li>)}
+      {coins.map((coin) => (
+      <li>
+        {coin.name} ({coin.symbol}) : {coin.quotes.USD.price} USD
+        </li>
+      ))}
     </ul>
-  </div>);
+  </div>
+  );
 }
-
 
 export default App;
 
@@ -61,3 +55,31 @@ function App() {
   );
 }
 */
+
+
+/*
+function App() {
+  const [toDo,setToDo] = useState("");
+  const [toDos, setToDos] = useState([]);
+  const onChange = (event) => setToDo(event.target.value);
+  const onSubmit = (event) => {
+    event.preventDefault();
+    if (toDo === ""){
+      return;
+    }
+    setToDos((currentArray) => [toDo, ...currentArray]);
+    setToDo("");
+  }
+  return (
+  <div>
+    <h1>My To Dos ({toDos.length})</h1>
+    <form onSubmit={onSubmit}>
+      <input onChange = {onChange} value = {toDo} type="text" placeholder="Write your to do"/>
+      <button>Add To Do</button>
+    </form>
+    <hr />
+    <ul>
+      {toDos.map((item, index) => <li key = {index}>{item}</li>)}
+    </ul>
+  </div>);
+}*/
