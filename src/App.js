cdb2e2a6e4721670f09ed1d7f6443b601ac87bc0
kth <0307rnxogh@daum.net>
2024-01-11 15:54:27 +0900

    React.todos

--- src/App.js
+++ src/App.js
@@ -1,6 +1,32 @@
 import {useState, useEffect} from "react";
 
+function App() {
+  const [toDo,setToDo] = useState("");
+  const [toDos, setToDos] = useState([]);
+  const onChange = (event) => setToDo(event.target.value);
+  const onSubmit = (event) => {
+    event.preventDefault();
+    if (toDo === ""){
+      return;
+    }
+    setToDos(currentArray => [toDo, ...currentArray]);
+    setToDo("");
+  }
+  return (
+  <div>
+    <h1>My To Dos ({toDos.length})</h1>
+    <form onSubmit={onSubmit}>
+      <input onChange = {onChange} value = {toDo} type="text" placeholder="Write your to do"/>
+      <button>Add To Do</button>
+    </form>
+  </div>);
+}
 
+
+export default App;
+
+
+/*
 function App() {
   const [counter,setValue] = useState(0);
   const [keyword, setKeyword] = useState("")
@@ -30,5 +56,4 @@ function App() {
     </div>
   );
 }
-
-export default App;
+*/
