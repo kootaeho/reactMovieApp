df4c44ee67bb27ebd9e062e709a414944b9b0b25
kootaeho <0307rnxogh@daum.net>
2024-01-14 18:06:45 +0900

    React.routerFolder

--- src/App.js
+++ src/App.js
@@ -1,29 +1,5 @@
-import { useEffect, useState } from "react";
-import Movie from "./Movie";
-
 function App(){
-  const [loading, setLoading] = useState(true);
-  const [movies,setMovies] = useState([]);
-  const getMovies = async() => {
-    const response = await fetch('https://yts.mx/api/v2/list_movies.json?minimum_rating=9&sort_by=year');
-    const json = await response.json();
-    setMovies(json.data.movies);
-    setLoading(false);
-  };
-  useEffect(() => {
-    getMovies();
-  }, []);
-  console.log(movies);
-  return <div>
-    {loading ? <h1>Loading...</h1> : (movies.map((movie) => 
-    <Movie 
-    key={movie.id}
-    title={movie.title} 
-    coverimg={movie.medium_cover_image} 
-    summary={movie.summary}
-    genres={movie.genres} />))
-    }
-  </div>;
+  return null;
 }
 
 
---
+++ src/routes/Home.js
@@ -0,0 +1,32 @@
+import { useEffect, useState } from "react";
+import Movie from "../components/Movie";
+
+function Home(){
+    const [loading, setLoading] = useState(true);
+  const [movies,setMovies] = useState([]);
+  const getMovies = async() => {
+    const response = await fetch('https://yts.mx/api/v2/list_movies.json?minimum_rating=9&sort_by=year');
+    const json = await response.json();
+    setMovies(json.data.movies);
+    setLoading(false);
+  };
+  
+  useEffect(() => {
+    getMovies();
+  }, []);
+  console.log(movies);
+  return <div>
+    {loading ? <h1>Loading...</h1> : (movies.map((movie) => 
+    <Movie 
+    key={movie.id}
+    title={movie.title} 
+    coverimg={movie.medium_cover_image} 
+    summary={movie.summary}
+    genres={movie.genres} />))
+    }
+  </div>;
+
+}
+
+
+export default Home;
