0e5c901f0e5121d84111a73aac1050ccd309ad46
kootaeho <0307rnxogh@daum.net>
2024-01-14 17:13:44 +0900

    React.MovieAPI

--- src/App.js
+++ src/App.js
@@ -1,35 +1,30 @@
-import {useState, useEffect} from "react";
+import { useEffect, useState } from "react";
 
 function App(){
   const [loading, setLoading] = useState(true);
-  const [coins, setCoins] = useState([]);
-  const [userInput, setinput] = useState(0);
-  useEffect(() => {
-  fetch("https://api.coinpaprika.com/v1/tickers").then(response => response.json()).then((json) => {setCoins(json); setLoading(false)});
-  }, [])
-  const onChange = (event) => {
-    setinput(event.target.value);
+  const [movies,setMovies] = useState([]);
+  const getMovies = async() => {
+    const response = await fetch('https://yts.mx/api/v2/list_movies.json?minimum_rating=9&sort_by=year');
+    const json = await response.json();
+    setMovies(json.data.movies);
+    setLoading(false);
   };
-  return ( 
-  <div>
-    <h1>The Coins</h1>
-    {loading ? <strong>Loading...</strong>:null}
-    <input value = {userInput} type = "number" onChange = {onChange} placeholder = "USD 값을 입력하세요"/>
-
-
-    <ul>
-      {coins.map((coin) => (
-      <><h1>
-          {userInput / coin.quotes.USD.price} {coin.name}
-        </h1>
-        <li>
-            {coin.name} ({coin.symbol}) : {coin.quotes.USD.price} USD
-          </li></>
-      ))}
-    </ul>
-  </div>
-  );
+  useEffect(() => {
+    getMovies();
+  }, []);
+  console.log(movies);
+  return <div>
+    {loading ? <h1>Loading...</h1> : movies.map(movie => <div key = {movie.id}>
+      <h2>{movie.title}</h2>
+      <img src={movie.medium_cover_image}/>
+      <p>{movie.summary}</p>
+      <ul>
+        {movie.genres.map(g => <li key = {g}>{g}</li>)}
+      </ul>
+      </div>)}
+  </div>;
 }
+
 
 export default App;
 
@@ -93,3 +88,37 @@ function App() {
     </ul>
   </div>);
 }*/
+
+/*import {useState, useEffect} from "react";
+
+function App(){
+  const [loading, setLoading] = useState(true);
+  const [coins, setCoins] = useState([]);
+  const [userInput, setinput] = useState(0);
+  useEffect(() => {
+  fetch("https://api.coinpaprika.com/v1/tickers").then(response => response.json()).then((json) => {setCoins(json); setLoading(false)});
+  }, [])
+  const onChange = (event) => {
+    setinput(event.target.value);
+  };
+  return ( 
+  <div>
+    <h1>The Coins</h1>
+    {loading ? <strong>Loading...</strong>:null}
+    <input value = {userInput} type = "number" onChange = {onChange} placeholder = "USD 값을 입력하세요"/>
+
+
+    <ul>
+      {coins.map((coin) => (
+       <><h1>
+          {userInput / coin.quotes.USD.price} {coin.name}
+        </h1>
+        <li>
+            {coin.name} ({coin.symbol}) : {coin.quotes.USD.price} USD
+          </li></>
+      ))}
+    </ul>
+  </div>
+  );
+}
+*/
