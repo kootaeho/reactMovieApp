6da30e921038c399c5b3908978faae2cbc5ab490
kth <0307rnxogh@daum.net>
2024-01-17 20:05:40 +0900

    React.htmlFixed

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App(){
   <Router>
     <Routes>
     <Route path= "/movie/:id"  element = {<Detail/>}/>
-    <Route path = {process.env.PUBLIC_URL+"/"} element = {<Home/>}/>  
+    <Route path = "/" element = {<Home/>}/>  
     </Routes>
   </Router>
   );
