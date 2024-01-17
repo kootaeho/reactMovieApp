278788c6d57bd4b152ed7e83abc1b2df955a04a2
kootaeho <0307rnxogh@daum.net>
2024-01-17 18:49:53 +0900

    React.publish

--- src/App.js
+++ src/App.js
@@ -8,13 +8,12 @@ import Home from "./routes/Home";
 import Detail from "./routes/Detail";
 
 
-
 function App(){
   return (
   <Router>
     <Routes>
-    <Route path="/movie/:id" element = {<Detail/>}/>
-    <Route path = "/" element = {<Home/>}/>  
+    <Route path= "/movie/:id"  element = {<Detail/>}/>
+    <Route path = {process.env.PUBLIC_URL+"/"} element = {<Home/>}/>  
     </Routes>
   </Router>
   );
