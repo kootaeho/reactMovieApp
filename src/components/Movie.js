b17eb5f275465df299176d34361d867fc2ba3f02
kootaeho <0307rnxogh@daum.net>
2024-01-19 00:43:23 +0900

    React.cssFixed

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,15 +1,15 @@
 import PropTypes from "prop-types";
 import {Link} from "react-router-dom";
+import styles from './Movie.module.css';
 
 function Movie({id,title, coverimg, summary, genres}){
-
     return (
-    <div >  
+    <div className = {styles.con} >  
+        <img className = {styles.movie__img} src={coverimg} alt={title}/>
         <h2>
           <Link to = {`/movie/${id}`}>{title}</Link>
           </h2>
-        <img src={coverimg} alt={title}/>
-        <p>{summary}</p>
+        <p>{summary.length > 400 ? `${summary.slice(0, 400)}...`: summary}</p>
         <ul>
           {genres.map(g => <li key = {g}>{g}</li>)}
         </ul>
